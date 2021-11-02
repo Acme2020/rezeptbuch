ea9d4aa4c69c101a18156dde9ff9c190e94a440e
Acme2020 <stefandrescher82@googlemail.com>
2021-11-02 21:47:58 +0100

    Fixed routing, added bootstrap, added index page showing all recipes, added show page for single recipe, added respective methods in controller, started development of new reciped creation

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -3,5 +3,30 @@ class RecipesController < ApplicationController
   def index
     @recipes = Recipe.all
   end
-  
+
+
+    def show
+    @recipe = Recipe.find(params[:id])
+  end
+
+
+    def new
+        @recipe = Recipe.new
+    end
+
+    def create
+
+        @recipe = Recipe.new(recipe_params)
+
+        if @recipe.save 
+
+        else
+
+          flash[:alert] = "Rezept konnte nicht erstellt werden." 
+          render :action => 'new'
+
+        end
+
+    end
+
 end

b78644d6a09a05ff94189d4e0d56483dff28914b
Acme2020 <stefandrescher82@googlemail.com>
2021-11-04 14:58:06 +0100

    made recipe creation work from frontend, fixed layout issues in recipe index and show pages, show images from active stoarge and resize them, added create recipe button to index page

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,6 +1,6 @@
 class Recipe < ApplicationRecord
   has_one :author
   has_one_attached :image
-  has_many :ingredients
-  has_many :ratings, dependent: :destroy
+  has_many :ingredients, dependent: :delete_all
+  has_many :ratings, dependent: :delete_all
 end
